8f292196a7603c7ccff48e3b1fff702ee323fab3
funcelot <artur.mustafin@gmail.com>
2020-12-25 09:49:15 +0000

    simplify code spare

--- lisp.h
+++ lisp.h
@@ -9,30 +9,4 @@ typedef struct str {
     void (*free)(str_context);
 } str;
 
-typedef void * const readonly_ptr;
-typedef struct list * const readonly_list_ptr;
-typedef struct list {
-    /* points to previous node */
-    readonly_list_ptr prev;
-    /* payload */
-    readonly_ptr payload;
-} list;
-
-struct list_vtable {
-    /* initialize context */
-    readonly_list_ptr (*init)();
-    /* push item on current context (stack) */
-    readonly_list_ptr (*push)(readonly_list_ptr const current, void* item);
-    /* pop item on current context (stack) */
-    void* (*pop)(readonly_list_ptr const current);
-    /* destroy context */
-    void (*destroy)(readonly_list_ptr const current);
-    /* print head */
-    void (*print_head)(readonly_list_ptr const current);
-    /* print list */
-    void (*print)(readonly_list_ptr const current);
-};
-
-const struct list_vtable list_vt;
-
 #endif
